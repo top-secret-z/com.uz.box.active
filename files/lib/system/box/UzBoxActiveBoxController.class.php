60d2a53f95a0c0dc2bb434f609a238c1966a8001
Sascha Greuel <github@1-2.dev>
2022-09-18 22:35:00 +0200

    5.4.2

--- files/lib/system/box/UzBoxActiveBoxController.class.php
+++ files/lib/system/box/UzBoxActiveBoxController.class.php
@@ -1,100 +1,122 @@
 <?php
+
+/*
+ * Copyright by Udo Zaydowicz.
+ * Modified by SoftCreatR.dev.
+ *
+ * License: http://opensource.org/licenses/lgpl-license.php
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 namespace wcf\system\box;
+
 use wcf\system\cache\builder\UzBoxActiveCacheBuilder;
-use wcf\system\box\AbstractDatabaseObjectListBoxController;
 use wcf\system\event\EventHandler;
 use wcf\system\request\LinkHandler;
 use wcf\system\WCF;
 
 /**
  * Most active box controller.
- *
- * @author		2018-2022 Zaydowicz
- * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @package		com.uz.box.active
  */
-class UzBoxActiveBoxController extends AbstractDatabaseObjectListBoxController {
-	/**
-	 * @inheritDoc
-	 */
-	protected $conditionDefinition = 'com.uz.box.active.condition';
-	
-	// all positions
-	
-	/**
-	 * @inheritDoc
-	 */
-	public $defaultLimit = 5;
-	public $maximumLimit = 100;
-	
-	/**
-	 * @inheritDoc
-	 */
-	protected $sortFieldLanguageItemPrefix = 'com.uz.box.active';
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function getLink() {
-		if (MODULE_MEMBERS_LIST) {
-			$parameters = 'sortField=activityPoints&sortOrder=DESC';
-			
-			return LinkHandler::getInstance()->getLink('MembersList', [], $parameters);
-		}
-		
-		return '';
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	protected function getObjectList() {
-		// get conditions as parameters for cache builder
-		$parameters = [];
-		foreach ($this->box->getConditions() as $condition) {
-			$parameters[] = $condition->conditionData;
-		}
-		$parameters[] = ['limit' => $this->limit];
-		
-		$userList = UzBoxActiveCacheBuilder::getInstance()->getData($parameters);
-		
-		return $userList;
-	
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	protected function getTemplate() {
-		return WCF::getTPL()->fetch('boxUzActive', 'wcf', [
-				'boxUserList' => $this->objectList
-		], true);
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function hasContent() {
-		if ($this->objectList === null) {
-			$this->objectList = $this->getObjectList();
-		}
-		
-		EventHandler::getInstance()->fireAction($this, 'hasContent');
-		
-		return ($this->objectList !== null && count($this->objectList) > 0);
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	protected function loadContent() {
-		$this->content = $this->getTemplate();
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function hasLink() {
-		return MODULE_MEMBERS_LIST == 1;
-	}
+class UzBoxActiveBoxController extends AbstractDatabaseObjectListBoxController
+{
+    /**
+     * @inheritDoc
+     */
+    protected $conditionDefinition = 'com.uz.box.active.condition';
+
+    // all positions
+
+    /**
+     * @inheritDoc
+     */
+    public $defaultLimit = 5;
+
+    public $maximumLimit = 100;
+
+    /**
+     * @inheritDoc
+     */
+    protected $sortFieldLanguageItemPrefix = 'com.uz.box.active';
+
+    /**
+     * @inheritDoc
+     */
+    public function getLink()
+    {
+        if (MODULE_MEMBERS_LIST) {
+            $parameters = 'sortField=activityPoints&sortOrder=DESC';
+
+            return LinkHandler::getInstance()->getLink('MembersList', [], $parameters);
+        }
+
+        return '';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getObjectList()
+    {
+        // get conditions as parameters for cache builder
+        $parameters = [];
+        foreach ($this->box->getConditions() as $condition) {
+            $parameters[] = $condition->conditionData;
+        }
+        $parameters[] = ['limit' => $this->limit];
+
+        return UzBoxActiveCacheBuilder::getInstance()->getData($parameters);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getTemplate()
+    {
+        return WCF::getTPL()->fetch('boxUzActive', 'wcf', [
+            'boxUserList' => $this->objectList,
+        ], true);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function hasContent()
+    {
+        if ($this->objectList === null) {
+            $this->objectList = $this->getObjectList();
+        }
+
+        EventHandler::getInstance()->fireAction($this, 'hasContent');
+
+        return $this->objectList !== null && \count($this->objectList) > 0;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function loadContent()
+    {
+        $this->content = $this->getTemplate();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function hasLink()
+    {
+        return MODULE_MEMBERS_LIST == 1;
+    }
 }
--- files/lib/system/cache/builder/UzBoxActiveCacheBuilder.class.php
+++ files/lib/system/cache/builder/UzBoxActiveCacheBuilder.class.php
@@ -1,85 +1,107 @@
 <?php
+
+/*
+ * Copyright by Udo Zaydowicz.
+ * Modified by SoftCreatR.dev.
+ *
+ * License: http://opensource.org/licenses/lgpl-license.php
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 namespace wcf\system\cache\builder;
-use wcf\data\user\User;
-use wcf\system\cache\builder\AbstractCacheBuilder;
+
 use wcf\system\cache\runtime\UserProfileRuntimeCache;
 use wcf\system\database\util\PreparedStatementConditionBuilder;
 use wcf\system\WCF;
 
 /**
  * Caches the users with most activity points iaw conditions.
- * 
- * @author		2018-2022 Zaydowicz
- * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @package		com.uz.box.active
  */
-class UzBoxActiveCacheBuilder extends AbstractCacheBuilder {
-	/**
-	 * @inheritDoc
-	 */
-	protected $maxLifetime = 180;
-	
-	/**
-	 * @inheritDoc
-	 */
-	protected function rebuild(array $parameters) {
-		/**
-		 * preset data
-		 */
-		$sqlLimit = 0;
-		$conditionBuilder = new PreparedStatementConditionBuilder();
-		
-		foreach ($parameters as $condition) {
-			if (isset($condition['limit'])) {
-				$sqlLimit = $condition['limit'];
-			}
-			
-			if (isset($condition['lastActivity'])) {
-				$conditionBuilder->add('lastActivityTime > ?', [TIME_NOW - $condition['lastActivity'] * 86400]);
-			}
-			
-			if (isset($condition['userIsBanned'])) {
-				$conditionBuilder->add('banned = ?', [$condition['userIsBanned']]);
-			}
-			
-			if (isset($condition['userIsEnabled'])) {
-				if ($condition['userIsEnabled'] == 0) $conditionBuilder->add('activationCode > ?', [0]);
-				if ($condition['userIsEnabled'] == 1) $conditionBuilder->add('activationCode = ?', [0]);
-			}
-			
-			if (isset($condition['groupIDs'])) {
-				$conditionBuilder->add('userID IN (SELECT userID FROM wcf'.WCF_N.'_user_to_group WHERE groupID IN (?))', [$condition['groupIDs']]);
-			}
-			
-			if (isset($condition['notGroupIDs'])) {
-				$conditionBuilder->add('userID NOT IN (SELECT userID FROM wcf'.WCF_N.'_user_to_group WHERE groupID IN (?))', [$condition['notGroupIDs']]);
-			}
-		}
-		
-		// must have points
-		$conditionBuilder->add('activityPoints > ?', [0]);
-		
-		// get users
-		$userIDs = [];
-		$sql = "SELECT		userID
-				FROM		wcf".WCF_N."_user
-				".$conditionBuilder."
-				ORDER BY activityPoints DESC";
-		
-		$statement = WCF::getDB()->prepareStatement($sql, $sqlLimit);
-		$statement->execute($conditionBuilder->getParameters());
-		while ($row = $statement->fetchArray()) {
-			$userIDs[] = $row['userID'];
-		}
-		
-		$users = [];
-		if (!empty($userIDs)) {
-			foreach ($userIDs as $userID) {
-				$user = UserProfileRuntimeCache::getInstance()->getObject($userID);
-				$users[] = $user;
-			}
-		}
-		
-		return $users;
-	}
+class UzBoxActiveCacheBuilder extends AbstractCacheBuilder
+{
+    /**
+     * @inheritDoc
+     */
+    protected $maxLifetime = 180;
+
+    /**
+     * @inheritDoc
+     */
+    protected function rebuild(array $parameters)
+    {
+        /**
+         * preset data
+         */
+        $sqlLimit = 0;
+        $conditionBuilder = new PreparedStatementConditionBuilder();
+
+        foreach ($parameters as $condition) {
+            if (isset($condition['limit'])) {
+                $sqlLimit = $condition['limit'];
+            }
+
+            if (isset($condition['lastActivity'])) {
+                $conditionBuilder->add('lastActivityTime > ?', [TIME_NOW - $condition['lastActivity'] * 86400]);
+            }
+
+            if (isset($condition['userIsBanned'])) {
+                $conditionBuilder->add('banned = ?', [$condition['userIsBanned']]);
+            }
+
+            if (isset($condition['userIsEnabled'])) {
+                if ($condition['userIsEnabled'] == 0) {
+                    $conditionBuilder->add('activationCode > ?', [0]);
+                }
+                if ($condition['userIsEnabled'] == 1) {
+                    $conditionBuilder->add('activationCode = ?', [0]);
+                }
+            }
+
+            if (isset($condition['groupIDs'])) {
+                $conditionBuilder->add('userID IN (SELECT userID FROM wcf' . WCF_N . '_user_to_group WHERE groupID IN (?))', [$condition['groupIDs']]);
+            }
+
+            if (isset($condition['notGroupIDs'])) {
+                $conditionBuilder->add('userID NOT IN (SELECT userID FROM wcf' . WCF_N . '_user_to_group WHERE groupID IN (?))', [$condition['notGroupIDs']]);
+            }
+        }
+
+        // must have points
+        $conditionBuilder->add('activityPoints > ?', [0]);
+
+        // get users
+        $userIDs = [];
+        $sql = "SELECT        userID
+                FROM        wcf" . WCF_N . "_user
+                " . $conditionBuilder . "
+                ORDER BY activityPoints DESC";
+
+        $statement = WCF::getDB()->prepareStatement($sql, $sqlLimit);
+        $statement->execute($conditionBuilder->getParameters());
+        while ($row = $statement->fetchArray()) {
+            $userIDs[] = $row['userID'];
+        }
+
+        $users = [];
+        if (!empty($userIDs)) {
+            foreach ($userIDs as $userID) {
+                $user = UserProfileRuntimeCache::getInstance()->getObject($userID);
+                $users[] = $user;
+            }
+        }
+
+        return $users;
+    }
 }
